93e03afb89a5b1c57a618e644821abfe93791329
OKUMURA <s.okumura@iaea.org>
2023-08-08 13:13:12 +0200

    change structure

---
+++ src/exfor_dictionary/config.py
@@ -0,0 +1,30 @@
+####################################################################
+#
+# This file is part of exfor-parser.
+# Copyright (C) 2022 International Atomic Energy Agency (IAEA)
+#
+# Disclaimer: The code is still under developments and not ready
+#             to use. It has been made public to share the progress
+#             among collaborators.
+# Contact:    nds.contact-point@iaea.org
+#
+####################################################################
+import os
+
+DICTIONARY_URL = "https://nds.iaea.org/nrdc/ndsx4/trans/dictionaries/"
+
+
+if os.path.exists("trans_backup"):
+    DICTIONARY_PATH = ""
+
+else:
+    from importlib.resources import files
+    DICTIONARY_PATH = files("exfor_dictionary")
+
+print(DICTIONARY_PATH)
+
+
+PICKLE_PATH = os.path.join(DICTIONARY_PATH, "pickles")
+
+
+
